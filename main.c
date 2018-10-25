2c16af2011134ab79f34fd2527b39da6188280bc
iahmedbacha <fi_ahmed_bacha@esi.dz>
2018-10-25 21:22:39 +0100

    print articulation points

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printListeTable (int n, int** array);
+void bfs(int n, int u, int** adjacencyList, int* count, int* order, int* parent, int* children, int* low, int* ap);
+void printAdjacencyList (int n, int** adjacencyList);
 void printArray(int n, int* array);
-void bfs (int n, int** array, int i, int* alreadyVisited, int* visited, int* parent, int* returnArete);
 
 int main(int argc, char const *argv[]) {
 	// read input
@@ -14,67 +14,93 @@ int main(int argc, char const *argv[]) {
     }
     int n;
     fscanf(file,"%d",&n);
-    int* array[n];
+    int* adjacencyList[n];
     for (int i = 0; i < n; ++i) {
     	int nb;
     	fscanf(file,"%d",&nb);
-    	int* line = (int*) malloc(sizeof(int)*(nb+1));
-    	line[0]=nb;
+    	int* list = (int*) malloc(sizeof(int)*(nb+1));
+    	list[0]=nb;
 	    for (int j = 1; j < nb+1; ++j) {
-	    	fscanf(file,"%d",&line[j]);
+	    	fscanf(file,"%d",&list[j]);
 	    }
-	    array[i]=line;
+	    adjacencyList[i]=list;
     }
     fclose(file);
     // end read input
-    int* alreadyVisited = (int*) malloc(sizeof(int));
-    alreadyVisited=0;
-    int* visited = (int*) malloc(sizeof(int)*n); // if visited then 0 else order of visit 
+    // init structures
+    int count=0; // number of vertices already visited
+    int order[n]; // if visited then 0 else order of visit
+    int parent[n]; // if root then -1 else parent number 
+    int children[n]; // number of children 
+    int low[n]; // lowest return order in the subtree
+    int ap[n]; // if articulation point then 1 else 0
     for (int i = 0; i < n; ++i) {
-    	visited[i]=0;
+    	order[i]=0;
+    	parent[i]=-1;
+    	children[i]=0;
+    	low[i]=-1;
+    	ap[i]=0;
     }
-    int* parent = (int*) malloc(sizeof(int)*n);
-    parent[0]=0; // root
+    // end init structures
+
+    // bfs to detect cut vertices
     for (int i = 0; i < n; ++i) {
-    	visited[i]=-1;
+    	if (order[i]==0) { // not visited
+    		bfs(n, i, adjacencyList, &count, order, parent, children, low, ap);
+    		if (children[i]>1) {
+    			ap[i]=1;
+    		}
+    	}
     }
-    int* returnArete = (int*) malloc(sizeof(int)*n); 
-    for (int i = 0; i < n; ++i) {
-    	returnArete[i]=-1;
-    }
-    printListeTable(n, array);
-    bfs(n, array, 0, alreadyVisited, visited, parent, returnArete);
-    printArray(n, visited);
+    // end bfs
+
+    // print
+    printf("Adjacency list :\n");
+    printAdjacencyList(n, adjacencyList);
+    printf("Orders : \n");
+    printArray(n, order);
+    printf("Parents : \n");
     printArray(n, parent);
-    printArray(n, returnArete);
+    printf("Articulation points :\n");
+    for (int i = 0; i < n; ++i) {
+    	if (ap[i]!=0) {
+    		printf("%d\t", i);
+    	}
+    }
+    printf("\n");
+    // end print
 
 	return 0;
 }
 
-void bfs (int n, int** array, int i, int* alreadyVisited, int* visited, int* parent, int* returnArete) {
-	visited[i]=*alreadyVisited+1;
-	alreadyVisited++;
-	for (int j = 0; j < n; ++j) {
-		for (int k = 1; k < array[i][0]+1; ++k) {
-			if (visited[array[i][k]]>0) {
-				// voisin visité ==> arête de retour
-				returnArete[array[i][k]]=i;
+void bfs(int n, int u, int** adjacencyList, int* count, int* order, int* parent, int* children, int* low, int* ap) {
+	order[u]=++(*count);
+	low[u]=order[u];
+	for (int i = 1; i <= adjacencyList[u][0]; ++i) {
+		int v = adjacencyList[u][i];
+		if (order[v]==0) { // not visited
+			children[u]++;
+			if (u==0) {
+				printf("vertice 0 parent of %d\n", v);
 			}
-			else {
-				// voisin non visité
-				parent[array[i][k]]=i;
-				bfs(n, array, array[i][k], alreadyVisited, visited, parent, returnArete);
+			parent[v]=u;
+			bfs(n, v, adjacencyList, count, order, parent, children, low, ap);
+			low[u]=(low[u]>low[v])?low[v]:low[u];
+			if ((parent[u]!=-1)&&(low[v]>=order[u])) {
+				ap[u]=1;
 			}
+		}
+		else {
+			low[u]=(low[u]>order[v])?order[v]:low[u];
 		}
 	}
 }
 
-void printListeTable (int n, int** array) {
-	printf("n = %d\n", n);
+void printAdjacencyList (int n, int** adjacencyList) {
 	for (int i = 0; i < n; ++i) {
-		printf("noeud[%d]", i);
-		for (int j = 1; j < array[i][0]+1; ++j) {
-			printf("\t%d", array[i][j]);
+		printf("%d:\t", i);
+		for (int j = 1; j < adjacencyList[i][0]+1; ++j) {
+			printf("%d\t", adjacencyList[i][j]);
 		}
 		printf("\n");
 	}
@@ -82,6 +108,11 @@ void printListeTable (int n, int** array) {
 
 void printArray (int n, int* array) {
 	for (int i = 0; i < n; ++i) {
-		printf("\t%d\n", array[i]);
+		printf("%d\t", i);
 	}
+	printf("\n");
+	for (int i = 0; i < n; ++i) {
+		printf("%d\t", array[i]);
+	}
+	printf("\n");
 }
